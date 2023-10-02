6d279448941541044c7bc43d319e42de04d62779
Maciej Kusz <9740637+mkusz@users.noreplay.github.com>
2023-10-02 12:08:46 +0200

    Rollback site_url setup in blog creators

--- mkdocs_publisher/blog/creators.py
+++ mkdocs_publisher/blog/creators.py
@@ -203,14 +203,10 @@ def _render_and_write_page(
     # print(templates.list_templates())
     # template = templates.get_template("index.html")
 
-    site_url = ""
-    if blog_config.mkdocs_config.site_url is not None:
-        site_url = str(blog_config.mkdocs_config.site_url)
-
     index_template = resources.read_template_file(template_file_name="posts-list.html")
     context = {
         "posts": single_posts_chunk,
-        "site_url": site_url,
+        "site_url": str(blog_config.mkdocs_config.site_url),
         "config": blog_config.plugin_config,
         "translation": blog_config.translation,
     }
